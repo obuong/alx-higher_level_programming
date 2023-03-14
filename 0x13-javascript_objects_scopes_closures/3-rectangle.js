5a8191d35357d6b5c3a451f343758105a4c113a8
obuong <iobuong2021@gmail.com>
2023-03-14 21:04:25 +0300

    class notation

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+print () {
+for (let i = 0; i < this.width; i++) {console.log('x'.repeat(this.height));
+}
+}
+}
+module.exports = Rectangle;
