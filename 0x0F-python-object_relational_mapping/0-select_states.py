226c8156b82ecddbce4eb613ae9e0cf29972c91a
Esau Obuong <iobuong2021@gmail.com>
2023-03-19 13:29:31 +0300

    selecting states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
-
-'''
-lists all states in the database hbtn_0e_0_usa
-'''
-import sys
+"""
+Lists all states from the database hbtn_0e_0_usa sorted in ascending order by
+states.id
+"""
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
     try:
-        connection = MySQLdb.connect(
+        conn = MySQLdb.connect(
             host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2],
             port=3306,
-            db=sys.argv[3]
+            user=mysql_username,
+            passwd=mysql_password,
+            db=db_name,
+            charset="utf8"
         )
-    except MySQLdb.Error:
-        print("connection error")
-    try:
-        cur = connection.cursor()
-        cur.execute("SELECT * FROM states ORDER BY states.id")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-    except MySQLdb.Error:
-        print("Failed")
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
     cur.close()
-    connection.close()
+    conn.close()
