86ad11b368090778e456013512eb8af7bb624b00
Esau Obuong <iobuong2021@gmail.com>
2023-03-30 15:25:03 -0700

    size of a http request body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"
+# send a request to an URL with curl and displays the size of the body of the response
+curl -s -w '%{size_download}' -o /dev/null "$1"
