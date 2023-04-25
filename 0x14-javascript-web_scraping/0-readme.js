5439ea961d4b3d4d750ba6e05773b105effcbacb
obuong <iobuong@gmail.com>
2023-04-25 09:07:15 +0300

    script that reads and prints the content of a file in js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// script that reads and prints the content of a file
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(data);
+});
+
