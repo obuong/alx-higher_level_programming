59f0714a37f8ca0eccaaf7cbb9ac9f3b2ade19c7
obuong <iobuong@gmail.com>
2023-04-26 09:36:46 +0300

    updated

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,33 +1,20 @@
 #!/usr/bin/node
+
+// printing the status code of a GET request
+
 const request = require('request');
-
-// Get the API URL from the command line arguments
-const apiUrl = process.argv[2];
-
-// Make a GET request to the API URL
-request(apiUrl, function (error, response, body) {
-  // If there's an error, print it and exit the script
-  if (error) {
-    console.error(error);
-    process.exit(1);
-  }
-
-  // Parse the JSON response body
-  const films = JSON.parse(body);
-
-  // Initialize a counter for the number of films with Wedge Antilles
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  const data = JSON.parse(body);
   let count = 0;
-
-  // Loop through each film
-  for (let i = 0; i < films.results.length; i++) {
-    const film = films.results[i];
-
-    // Check if Wedge Antilles is in the list of characters for this film
-    if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      count++;
+  for (let i = 0; i < data.results.length; i++) {
+    const chars = data.results[i].characters;
+    for (let k = 0; k < chars.length; k++) {
+      if (chars[k].includes('18')) {
+        count = count + 1;
+      }
     }
   }
-
-  // Print the final count
   console.log(count);
 });
